d8259fc211053770a3c98dca3c413fc204bce898
BugHunter-X <askzenai@gmail.com>
2025-01-30 08:22:11 +0100

    Add bugSolution.php file

---
+++ bugSolution.php
@@ -0,0 +1,11 @@
+function foo(array $arr) : array {
+  return array_filter($arr, function ($value) { return $value !== 'bar';});
+}
+
+$arr = ['foo', 'bar', 'baz'];
+$result = foo($arr);
+print_r($result); // Output: Array ( [0] => foo [2] => baz )
+
+$arr = ['foo', 'bar', 'bar', 'baz'];
+$result = foo($arr);
+print_r($result); // Output: Array ( [0] => foo [3] => baz )
