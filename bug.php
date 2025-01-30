c445d185edbd4b06e3f765c2cce2beeaaa801927
BugHunter-X <askzenai@gmail.com>
2025-01-30 08:22:10 +0100

    Add bug.php file

---
+++ bug.php
@@ -0,0 +1,16 @@
+function foo(array $arr) {
+  foreach ($arr as $key => $value) {
+    if ($value === 'bar') {
+      unset($arr[$key]);
+    }
+  }
+  return $arr;
+}
+
+$arr = ['foo', 'bar', 'baz'];
+$result = foo($arr);
+print_r($result); // Output: Array ( [0] => foo [2] => baz )
+
+$arr = ['foo', 'bar', 'bar', 'baz'];
+$result = foo($arr);
+print_r($result); // Output: Array ( [0] => foo [3] => baz ) //Incorrect, should be [0] => foo [2] => baz
